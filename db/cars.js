91ce9a51a57b75b4006459dcc6a70cef58bb289c
Kateti Mareko <katetimareko@codenertapps.com>
2021-04-02 11:46:09 +1300

    Added the get cars by type

--- db/cars.js
+++ db/cars.js
@@ -1,1 +1,11 @@
 const connection = require('./')
+
+const TABLE_NAME = "cars"
+
+function getCarsByType(type, db = connection) {
+    return db(TABLE_NAME).where('type_id', type)
+}
+
+module.export = {
+    getCarsByType
+}
