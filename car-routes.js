e7d371f921aa8444f6553338899dc433a9d80e02
Alexander McKerrow <mckerrow4@gmail.com>
2021-04-02 12:40:40 +1300

    homepage almost there

--- car-routes.js
+++ car-routes.js
@@ -6,36 +6,36 @@ const router = express.Router()
 
 //example 
 router.get('/', (req, res) => {
-   return types.getTypes()
-   .then(types => {
-       const viewData = {
-           types
-       }
-       console.log(types)
-       res.render('home', viewData)
-   })
-   .catch((Error) => {
-    console.log(Error.message)
-    res.send('it aint working :(')
-  })
-  })
+  return types.getTypes()
+    .then(types => {
+      const viewData = {
+        types
+      }
+      console.log(types)
+      res.render('home', viewData)
+    })
+    .catch((Error) => {
+      console.log(Error.message)
+      res.send('it aint working :(')
+    })
+})
 
-  router.get('/low/:id', (req, res) => {
-    const id = Number(req.params.id)
+router.get('/low/:id', (req, res) => {
+  const id = Number(req.params.id)
 
-  })
+})
 
-  router.get('/medium/:id', (req, res) => {
-    const id = Number(req.params.id)
+router.get('/medium/:id', (req, res) => {
+  const id = Number(req.params.id)
 
-  })
+})
 
-  router.get('/high/:id', (req, res) => {
-    const id = Number(req.params.id)
+router.get('/high/:id', (req, res) => {
+  const id = Number(req.params.id)
 
-  })
+})
 
-  module.exports = router
+module.exports = router
 
 //for all routes add .catch at the end 
 
