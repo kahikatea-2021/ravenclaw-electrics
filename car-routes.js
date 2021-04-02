c9302f761f40bfad17205841ada5e1853d41fe3a
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-04-02 13:35:34 +1300

    replaced faulty car-routes

--- car-routes.js
+++ car-routes.js
@@ -6,21 +6,20 @@ const router = express.Router()
 
 //example 
 router.get('/', (req, res) => {
-  return types.getTypes()
-    .then(types => {
-      const viewData = {
-        types
-      }
-      console.log(types)
-      res.render('home', viewData)
-    })
-    .catch((Error) => {
-      console.log(Error.message)
-      res.send('it aint working :(')
-    })
-})
+   return types.getTypes()
+   .then(types => {
+       const viewData = {
+           types
+       }
+       console.log(types)
+       res.render('home', viewData)
+   })
+   .catch((Error) => {
+    console.log(Error.message)
+    res.send('it aint working :(')
+  })
+  })
 
-<<<<<<< HEAD
   router.get('/types/:id', (req, res) => {
     const id = Number(req.params.id)
     return cars.getCarByType(id)
@@ -34,47 +33,14 @@ router.get('/', (req, res) => {
         console.log(Error.message)
         res.send('whers you cars mate??')
       })
-  })
-||||||| 91ddb71
-  router.get('/low/:id', (req, res) => {
-    const id = Number(req.params.id)
+  })c
 
-  })
-
-  router.get('/medium/:id', (req, res) => {
-    const id = Number(req.params.id)
-
-  })
-=======
-router.get('/low/:id', (req, res) => {
-  const id = Number(req.params.id)
-
-})
-
-router.get('/medium/:id', (req, res) => {
-  const id = Number(req.params.id)
-
-})
->>>>>>> 1f55851e22a795a376e5c4329286f80cc60d3b76
-
-<<<<<<< HEAD
   router.get('/car/:id', (req, res) => {
     const id = Number(req.params.id)
-    
-  })
-||||||| 91ddb71
-  router.get('/high/:id', (req, res) => {
-    const id = Number(req.params.id)
 
   })
-=======
-router.get('/high/:id', (req, res) => {
-  const id = Number(req.params.id)
 
-})
->>>>>>> 1f55851e22a795a376e5c4329286f80cc60d3b76
-
-module.exports = router
+  module.exports = router
 
 //for all routes add .catch at the end 
 
